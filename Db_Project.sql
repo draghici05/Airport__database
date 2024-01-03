e515444c20c7cc0066502434446d83c9cd69e364
draghici05 <alexdraghici190@gmail.com>
2024-01-04 00:46:52 +0200

    Modified tables

--- Db_Project.sql
+++ Db_Project.sql
@@ -1,34 +1,33 @@
 CREATE TABLE Airplane (
-    airplane_id INT CONSTRAINT pk_airplane_id PRIMARY KEY,
+    airplane_id number(3) CONSTRAINT pk_airplane_id PRIMARY KEY,
     manufacturer varchar(100),
     airplane_model varchar(100),
-    airplane_capacity INT,
-    production_year INT
+    airplane_capacity number(4),
+    production_year number(4)
 );
 
-
 CREATE TABLE Airlines(
-    airline_id INT PRIMARY KEY,
-    airline_name varchar(100) NOT NULL,
+    airline_id number(3) PRIMARY KEY,
+    airline_name varchar(100),
     country varchar(100)
 );
 
 CREATE TABLE Airport(
-    airport_name varchar2(100) NOT NULL,
-    airport_location varchar2(100) NOT NULL,
-    contact_nubmer varchar2(10) NOT NULL
+    airport_name varchar2(100),
+    airport_location varchar2(100),
+    contact_number number(10)
 );
 
 CREATE TABLE Flights(
-    flight_id INT PRIMARY KEY,
-    airline_id NUMBER(5) CONSTRAINT fk_airline_id REFERENCES Airlines(airline_id),
+    flight_id number(2) PRIMARY KEY,
+    airline_id number(5) CONSTRAINT fk_airline_id REFERENCES Airlines(airline_id),
     flight_number varchar2(10) NOT NULL,
     departure_airport varchar2(50) NOT NULL,
     arrival_airport varchar2(50) NOT NULL
 );
 
 CREATE TABLE Flight_Attendant(
-    attendant_id INT PRIMARY KEY, 
+    attendant_id number(3) PRIMARY KEY, 
     att_first_name varchar2(50),
     att_last_name varchar2(50),
     hire_date DATE
@@ -40,12 +39,11 @@ CREATE TABLE Countries(
     region_id number
 );
 
-CREATE TABLE Passengers(
-    passenger_id INT PRIMARY KEY,
-    pass_first_name varchar2(40),
-    pass_last_name varchar2(40),
-    birth_date DATE,
-    passaport_number number(9)
+CREATE TABLE Contracts(
+    contract_id number(3) PRIMARY KEY,
+    airline_id number(3) CONSTRAINT fk_airline_id REFERENCES Airlines(airline_id),
+    airport_name varchar2(100) CONSTRAING fk_airline_id REFERENCES Airport(airport_name),
+    contract_value number(2.2), --MILLIONS
+    con_start_date date,
+    con_end_date date
 );
-
-
